ba613c162ce0444162bb52d5e51f7a23f7553943
Ahmed Khan <ahmed5179khan@gmail.com>
2026-02-08 15:22:53 +0300

    Initial DevOps project

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'db',
+  database: 'devopsdb',
+  password: 'postgres',
+  port: 5432,
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.send(`Database connected at: ${result.rows[0].now}`);
+  } catch (err) {
+    res.send('Database not connected');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
