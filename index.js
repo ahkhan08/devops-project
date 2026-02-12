34eca8f422a39985f0da9c19d5f4690b46383154
Ahmed Khan <ahmed5179khan@gmail.com>
2026-02-12 14:49:46 +0300

    use env variables

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
 
 const pool = new Pool({
-  user: 'appuser',
-  host: 'mydb',
-  database: 'appdb',
-  password: 'strongpassword',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
 
 
 app.get('/', async (req, res) => {
